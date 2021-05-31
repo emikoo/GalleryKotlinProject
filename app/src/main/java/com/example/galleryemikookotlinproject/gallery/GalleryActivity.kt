0c149146d17e964e9027a4572ecf0ec2f66ea4ed
emikoo <emikoo@users.noreply.github.com>
2021-05-31 14:15:46 +0600

    added permission for images in gallery & perm dialog

--- app/src/main/java/com/example/galleryemikookotlinproject/gallery/GalleryActivity.kt
+++ app/src/main/java/com/example/galleryemikookotlinproject/gallery/GalleryActivity.kt
@@ -1,8 +1,11 @@
 package com.example.galleryemikookotlinproject.gallery
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.galleryemikookotlinproject.R
 import kotlinx.android.synthetic.main.activity_gallery.*
 
@@ -15,12 +18,34 @@ class GalleryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
 
-        setupRecyclerView()
+        getPermission()
     }
 
     private fun setupRecyclerView() {
         adapter = GalleryAdapter()
         recycler_view.adapter = adapter
+    }
 
+    private fun getPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            ) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1
+                )
+            }
+        } else {
+            setupRecyclerView()
+        }
     }
 }
